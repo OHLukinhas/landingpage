85c0a9f1e5fe01622a1aff4ed4e5a6d235270ec0
Lucas Gean <contato.lucasgean@gmail.com>
2024-04-25 18:40:27 -0300

    att

--- painel.php
+++ painel.php
@@ -1,16 +1,36 @@
+<?php
+
+include('protect.php');
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <!--link do bootstrap (pagina responsiva)-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!--fim do link bootstrap (pagina responsiva)-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style-painel.css">
     <!--link da fonte-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <title>To Do List</title>
+    <title>Lista de tarefas</title>
+
+
+
+
 </head>
+
+
 <body>
+    Bem vindo ao Painel de tarefas! <?php echo $_SESSION['nome']; ?>
+
+    <p>
+        <a href="logout.php">Sair</a>
+    </p>
     
     <div id="container">
         <h1 id="titulo">Suas Tasks</h1>
