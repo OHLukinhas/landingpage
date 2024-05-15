465c94bb65eb4896982b580c388445d3939630fe
Alison Rocha <alisongabrielrocha2@hotmail.com>
2024-05-14 22:02:56 -0300

    feito ajustes de bootstrap e algumas correções na index e painel

--- painel.php
+++ painel.php
@@ -7,15 +7,22 @@ include('protect.php');
 <html lang="pt-br">
 
 <head>
+
+    <?php echo $_SESSION['nome']; ?>
+    <!-- $_SESSION =  usado para armazenar informações temporárias,
+     como dados do usuário, preferências, autenticação e outros estados relevantes, 
+     podendo ser usada uma variavel de SESSION em todas as paginas -->
+
     <meta charset="utf-8">
-    <title>Menu de tarefas</title>
-    <!--link do bootstrap (pagina responsiva)-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!--link do  bootstrap (pagina responsiva)-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--viewport - vai ocupar a largura do dispositivo
+    device-width - se usar um dispositivo movel 
+    initial-scale - zom padrão dependendo do dispositivo
+    integrity - gera um chave no site do bootstrap para carregamento dos arquivos com segurança.
+-->
     <!--fim do link bootstrap (pagina responsiva)-->
 
 
@@ -34,8 +41,8 @@ include('protect.php');
 
 </head>
 
-
 <body>
+
     <!-- navbar do painel -->
     <nav class="navbar bg-dark fixed-top bg-body-tertiary " data-bs-theme="dark">
         <div class="container-fluid">
@@ -49,91 +56,82 @@ include('protect.php');
     </nav>
 
     <!-- fim da navbar do painel -->
-
-    <?php echo $_SESSION['nome']; ?>
     <!--inicio de formulario tarefas-->
-
-    <title>Formulário de Texto</title>
-    </head>
-
-    <body>
-
-        <!--inicio de formulario tarefas-->
-        <div id="container">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row align-items-start">
-                        <div class="col">
-                            Nova tarefa
-                        </div>
+    <div id="container">
+        <div class="card">
+            <div class="card-header">
+                <div class="row align-items-start">
+                    <div class="col">
+                        Nova tarefa
                     </div>
                 </div>
-                <!--inicio do container de titulo e descrição-->
-                <div id="caixa-formulario" class="container">
-                    <div class="mb-2">
-                        <label class="form-label">Titulo</label>
-                        <input type="text" class="form-control">
+            </div>
+            <!--inicio do container de titulo e descrição-->
+            <div id="caixa-formulario" class="form-group">
+                <div class="mb-2">
+                    <label class="form-label">Titulo</label>
+                    <input type="text" class="form-control" placeholder="Digite o Titulo">
+                </div>
+                <div>
+                    <label class="form-label">Descrição</label>
+                    <textarea class="form-control" rows="2" placeholder="Descreva a tarefa!"></textarea>
+                    <div id="botao-salvar">
+                        <button onclick="newTask()" type="button" class="btn btn-outline-success" title="Clique aqui para adicionar uma nova tarefa">Salvar</button>
                     </div>
-                    <div>
-                        <label class="form-label">Descrição</label>
-                        <textarea class="form-control" rows="2"></textarea>
-                        <div id="botao-salvar">
-                            <button onclick="newTask()" type="button" class="btn btn-outline-success" title="Clique aqui para adicionar uma nova tarefa">Salvar</button>
-                        </div>
-                    </div>
-                    <!--fim do container de titulo e descrição-->
+                </div>
+                <!--fim do container de titulo e descrição-->
 
-                    <!--inicio tarefas registradas-->
-                    <div id="container" class="container">
-                        <!--inicio colunas-->
+                <!--inicio tarefas registradas-->
+                <div id="container" class="container">
+                    <!--inicio colunas-->
 
-                        <div class="container-fluid">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">N</th>
-                                        <th scope="col">Nome</th>
-                                        <th scope="col">Descrição</th>
-                                        <th scope="col">Data</th>
-                                    </tr>
-                                </thead>
+                    <div class="container-fluid">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Tarefa</th>
+                                    <th scope="col">Data</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Ações</th>
+                                </tr>
+                            </thead>
 
-                                <table class="table table-hover">
-                                    <tbody>
-                                        <div>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Teste</td>
-                                                <td>Teste</td>
-                                                <td>30/04/2024</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">2</th>
-                                                <td>Teste</td>
-                                                <td>Teste</td>
-                                                <td>01/05/2024</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td>Teste</td>
-                                                <td>Teste</td>
-                                                <td>01/05/2024</td>
-                                            </tr>
-                                        </div>
-                                    </tbody>
-                                </table>
-
+                            <table class="table table-hover">
+                                <tbody>
+                                    <div>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Teste</td>
+                                            <td>30/04/2024</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Teste</td>
+                                            <td>01/05/2024</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Teste</td>
+                                            <td>01/05/2024</td>
+                                        </tr>
+                                    </div>
+                                </tbody>
                             </table>
-                            <div>
-                                <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 75%">75%</div>
-                                </div>
-                            </div>
-                            <!--fim colunas-->
 
-                            <!--fim tarefas registradas-->
-                        </div>
+                        </table>
+
+                        <!--fim colunas-->
+
+                        <!--fim tarefas registradas-->
                     </div>
-    </body>
+                </div>
+
+
+                <!--link do  bootstrap (pagina responsiva)-->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+                <!-- O Popper. js é uma biblioteca JavaScript que tem a função de posicionar elementos como menus, tooltips e popovers-->
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
 
 </html>
